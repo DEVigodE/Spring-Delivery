7e3787073165204f9af7fba1ac5905dabafefeaa
ZIBMBRAZIL <moreira_igor9@hotmail.com>
2021-02-15 00:47:04 -0300

    First Commit
    Spring Initializr

---
+++ src/main/java/com/zibmbrazil/springdelivery/SpringdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.zibmbrazil.springdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zibmbrazil/springdelivery/SpringdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zibmbrazil.springdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
