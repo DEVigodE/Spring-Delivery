c166205da704003d414bfdb1b04f54946d2d796e
ZIBMBRAZIL <moreira_igor9@hotmail.com>
2021-02-15 21:37:02 -0300

    Create endpoints …
    Create endpoints
     - [GET] /orders
    
    Criando endpoints
     - [GET] /orders

---
+++ BACK-END/src/main/java/com/zibmbrazil/springdelivery/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.zibmbrazil.springdelivery.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.zibmbrazil.springdelivery.dto.OrderDTO;
+import com.zibmbrazil.springdelivery.entities.Order;
+import com.zibmbrazil.springdelivery.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+
+	}
+
+}
