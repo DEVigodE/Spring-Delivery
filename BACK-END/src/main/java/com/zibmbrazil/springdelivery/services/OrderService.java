325479ea81f882e2240580462d8202d1ae0d6970
ZIBMBRAZIL <moreira_igor9@hotmail.com>
2021-02-15 23:07:40 -0300

    Create endpoints …
    Create endpoints
     - [POST] /orders
    
    Criando endpoints
     - [POST] /orders

--- BACK-END/src/main/java/com/zibmbrazil/springdelivery/services/OrderService.java
+++ BACK-END/src/main/java/com/zibmbrazil/springdelivery/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.zibmbrazil.springdelivery.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.zibmbrazil.springdelivery.dto.OrderDTO;
+import com.zibmbrazil.springdelivery.dto.ProductDTO;
 import com.zibmbrazil.springdelivery.entities.Order;
+import com.zibmbrazil.springdelivery.entities.Product;
+import com.zibmbrazil.springdelivery.enums.OrderStatus;
 import com.zibmbrazil.springdelivery.repositories.OrderRepository;
+import com.zibmbrazil.springdelivery.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,11 +22,27 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 
+	@Autowired
+	private ProductRepository productRepository;
+
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 
+	}
+
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(),
+				OrderStatus.PENDIND);
+		for (ProductDTO _productDto : dto.getProducts()) {
+			Product _product = productRepository.getOne(_productDto.getId());
+			order.getProducts().add(_product);
+		}
+		order = repository.save(order);
+
+		return new OrderDTO(order);
 	}
 
 }
